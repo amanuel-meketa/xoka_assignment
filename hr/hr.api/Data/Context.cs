e62508e6f896fc32e0811b1b5d7a203c22499fcf
Amanuel <amanuel.meketa@gmail.com>
2024-09-26 15:35:23 -0700

    add employee crud opration

---
+++ hr/hr.api/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using hr.api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {}
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
--- hr/hr.api/Program.cs
+++ hr/hr.api/Program.cs
@@ -1,9 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Context") ?? throw new InvalidOperationException("Connection string 'Context' not found.")));
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,8 +17,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
